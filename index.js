75d33dbbf09d91395fddd3027baf41085f5f8239
Muhammad Rudyn <nydurism@gmail.com>
2024-03-05 19:43:53 +0800

    index.js update for Mar 5

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-module.exports.handler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "THIS IS OUR HTTP RESPONSE!",
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
+module.exports.rudyn = async (event) => {
+  console.log(new Date() + " Second Last Session Of Cohort 4")
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "THIS IS OUR HTTP RESPONSE",
+      },
+      null,
+      2
+    ),
   };
+};
   
